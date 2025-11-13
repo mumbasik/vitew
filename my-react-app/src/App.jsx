46132f95e0d8662f90776da602c0bfab6aa45ef3
mumbasik <mumbasik4@gmail.com>
2025-11-13 03:24:56 +0200

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "50px",
+        border: "2px solid gray",
+        borderRadius: "10px",
+        width: "300px",
+        height: "100px",
+        boxShadow: "10px 10px 5px grey",
+      }}
+    >
+      <h1
+        style={{
+          color: "gray",
+          fontFamily:
+            '"Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif',
+          fontSize: "medium",
+        }}
+      >
+        My city, Odessa!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
